4a0b8a3ee58efc6f3b4dd9fd0814f30763d7fe5c
Fatima Garcia <fagarmartin@gmail.com>
2023-04-18 08:56:36 +0200

    Modificado

--- src/movies.js
+++ src/movies.js
@@ -79,67 +79,81 @@ function dramaMoviesScore(moviesArray) {
 // Iteration 5: Ordering by year - Order by year, ascending (in growing order)
 function orderByYear(moviesArray) {
   let newArray = JSON.parse(JSON.stringify(moviesArray));
-  newArray.sort((el1,el2)=>{
-    if(el1.year===el2.year)
-    {
-       // el1.title.localeCompare(el2)
-       if(el1.title<el2.title)
-       {
-        return -1
-       }
-       else if(el1.title>el2.title){
-        return 1
-
-       }
-       else{
+  newArray.sort((el1, el2) => {
+    if (el1.year === el2.year) {
+      // el1.title.localeCompare(el2)
+      if (el1.title < el2.title) {
+        return -1;
+      } else if (el1.title > el2.title) {
+        return 1;
+      } else {
         return 0;
-       }
+      }
+    } else {
+      return el1.year - el2.year;
     }
-    else{
-        return el1.year-el2.year
-    }
-       
-  })
+  });
   return newArray;
 }
 
 // Iteration 6: Alphabetic Order - Order by title and print the first 20 titles
 function orderAlphabetically(moviesArray) {
-    let count=0;
-    let newArray = JSON.parse(JSON.stringify(moviesArray));
-    let sortedArray=moviesArray.sort((el1,el2)=>{
-        if(el1.title<el2.title)
-        {
-            return -1;
-        }
-        else if(el2.title<el1.title)
-        {
-            return 1;
-        }
-        else{
-            return 0;
-        }
-       // return el1.title.localeCompare(el2.title)   
+  let count = 0;
+  let newArray = JSON.parse(JSON.stringify(moviesArray));
+  let sortedArray = moviesArray.sort((el1, el2) => {
+    if (el1.title < el2.title) {
+      return -1;
+    } else if (el2.title < el1.title) {
+      return 1;
+    } else {
+      return 0;
+    }
+    // return el1.title.localeCompare(el2.title)
+  });
 
-    })
+  //  console.log(sortedArray,"ORDEN ALFABETICO")
+  let copyArr = sortedArray.slice(0, 20);
 
+  let mappedArr = copyArr.map((element) => {
+    return element.title;
+  });
+  // mappedArr.sort()
+  // console.log(mappedArr,"copyArr")
 
-    console.log(sortedArray,"ORDEN ALFABETICO")
-    let copyArr=sortedArray.slice(0,20)
-   
-    let mappedArr=copyArr.map((element)=>{
-        return element.title;
-    })      
-   // mappedArr.sort()
-    console.log(mappedArr,"copyArr")
-  
- 
-    
-return mappedArr;
+  return mappedArr;
 }
 
 // BONUS - Iteration 7: Time Format - Turn duration of the movies from hours to minutes
-function turnHoursToMinutes(moviesArray) {}
+function turnHoursToMinutes(moviesArray) {
+  let copyArr = JSON.parse(JSON.stringify(moviesArray));
+  let newArray = copyArr.forEach((element) => {
+    let total = 0;
+    let hoursArr = element.duration.split("h");
+    let hours = 0;
+    if (hoursArr.length > 0) {
+      hours = hoursArr[0] * 60;
+    } else {
+      hours = 0;
+    }
+    //console.log(hours,"hours")
+    let minutesArr = element.duration.split("min");
+    let minutes = Number(minutesArr[0].slice(2));
+    if (minutes > 0 && hours > 0) {
+      total = minutes + hours;
+    } else {
+      total = hours;
+    }
+    total = minutes + hours;
+    // console.log(total,"total")
+    element.duration = total;
+    // console.log(element,"element");
+  });
+  //console.log(copyArr,"copyArr");
+  return copyArr;
+}
 
 // BONUS - Iteration 8: Best yearly score average - Best yearly score average
-function bestYearAvg(moviesArray) {}
+function bestYearAvg(moviesArray) {
+  
+  
+}
